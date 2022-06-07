d9078477159d432f06a4f5ffc31ee3383dd0bb1c
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-06-07 12:31:08 +0300

    Automatically flush CSS caches after changes

--- pelican/themes/smallweb/__init__.py
+++ pelican/themes/smallweb/__init__.py
@@ -2,7 +2,8 @@
 SmallWeb theme for Pelican static site generator
 '''
 
-from pkg_resources import resource_filename
+from pkg_resources import resource_filename, resource_string
+from hashlib import sha256 as hashfunc
 
 def path():
     '''
@@ -10,3 +11,17 @@ def path():
     Use this as value for THEME in Pelican settings
     '''
     return resource_filename(__name__, '')
+
+
+RESOURCES = [
+    '/css/fonts.css',
+    '/css/style.css',
+]
+def hashes():
+    return {name: _hash('static' + name) for name in RESOURCES}
+
+
+
+def _hash(filename):
+    checksum = hashfunc(resource_string(__name__, filename))
+    return checksum.hexdigest()
