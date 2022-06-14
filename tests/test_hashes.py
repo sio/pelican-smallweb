c5b53c3f785c4053ed2fdba04a5ed10c0a5704c0
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-06-14 12:25:51 +0300

    Update tests to support dynamic CSS lookup

--- tests/test_hashes.py
+++ tests/test_hashes.py
@@ -4,7 +4,9 @@ from pelican.themes import smallweb
 def test_calculate_hashes():
     '''Check that hash calculation does not fail'''
     result = smallweb.hashes()
-    assert len(result) == 2
-    for value in result.values():
+    assert len(result) >= 2
+    for key, value in result.items():
+        assert key.startswith('/')
+        assert key.endswith('.css')
         assert isinstance(value, str)
         assert len(value) == 64  # sha256
